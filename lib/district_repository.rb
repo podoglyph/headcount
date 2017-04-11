400cd100534aff29ecb410abd30d8287fc430cfb
JoelLindow <joellindow@gmail.com>
2017-04-11 08:20:55 -0600

    add test data to repository

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -1,11 +1,42 @@
+require_relative 'district.rb'
+require 'csv'
+require 'pry'
+require 'pry-state'
+
 class DistrictRepository
+  attr_reader :districts
 
+  def initialize
+    @districts = Hash.new
+  end
 
-  def load_data({
-    :enrollment => {
-      :kindergarten => "./data/Kindergartners in full-day program.csv"
-    }
-    })
+  def load_data(args)
+    contents = CSV.open args[:enrollment][:kindergarten], headers: true, header_converters: :symbol
+    @districts_list = extract_locations(contents)
+    # get a list of all locations that we can search
+  end
+
+  def extract_locations(contents)
+    #create a list we can search.
+    contents = contents.map do |x|
+      name = x[:location]
+      x = District.new(name)
+    end
+  end
+
+  def find_by_name(find_name)
+    #find a specific name in the list of districts
+    find_name = find_name.upcase
+    @districts_list.each do |district|
+      searchable_name = district.name.upcase
+      if find_name == searchable_name
+        return district
+      end
+    end
+  end
+
+  def find_all_matching(name)
+    #find_all_matching - returns either [] or one or more matches which contain the supplied name fragment, case insensitive
   end
 
 end
--- test/district_repository_test.rb
+++ test/district_repository_test.rb
@@ -1,19 +1,25 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/district_repository.rb'
+require_relative '../lib/district_repository.rb'
 
 class DistrictRepositoryTest < Minitest::Test
 
   def test_it_exists
     d = DistrictRepository.new
+
     assert_equal DistrictRepository, d.class
   end
 
-  def test_it_can_load_file
+  def test_load_districts
     d = DistrictRepository.new
-    # File.read('./data/').split("\n")
-    
+    d.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv"
+        }
+      })
+    district = d.find_by_name("ACADEMY 20")
+    assert_equal "ACADEMY 20" , district.name
+    assert_instance_of District, district
   end
-
 
 end
