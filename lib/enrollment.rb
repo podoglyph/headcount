866f85d3c47e69f53b18d4be7768efc603550048
Padraic O'Neill <pconeill@gmail.com>
2017-04-12 15:15:03 -0600

    create #kindergarten methods

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -9,4 +9,19 @@ class Enrollment
     @kindergarten_participation = enrollment_data[:kindergarten_participation]
   end
 
+
+  def kindergarten_participation_by_year
+    # enrollment.kindergarten_participation_by_year
+    # { 2010 => 0.391,
+    #  2011 => 0.353,
+    #  2012 => 0.267,
+    # }
+
+
+  end
+
+  def kindergarten_participation_in_year(year)
+
+  end
+
 end
