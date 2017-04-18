2b86ab20de19b980f58e54a88d80c3d5ae331072
Padraic O'Neill <pconeill@gmail.com>
2017-04-17 22:43:05 -0600

    working towards end of iteration one. check first run to see if data matches

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -5,10 +5,12 @@ class HeadcountAnalyst
     @repo = district_repository
   end
 
-  def kindergarten_participation_rate_variation(district_name, base)
+  def kindergarten_participation_rate_variation(district_name, base = nil)
     district = @repo.find_by_name(district_name)
     acquire_single_district_stats(district)
-    average_district_participation
+    base = average_statewide_aggregate_stats
+    result = average_district_participation / base
+    result = result.round(3)
   end
 
   def acquire_single_district_stats(district)
@@ -28,10 +30,21 @@ class HeadcountAnalyst
 
   def extract_aggregate_data
     aggregate_statewide_stats = []
-    acquire_all_districts.each_key do |k|
-      k.kindergarten_participation.each_value {|value| aggregate_statewide_stats << value }
+    acquire_all_districts.each_pair do |k, v|
+      v.kindergarten_participation.each_value {|value| aggregate_statewide_stats << value }
     end
-    @aggregate_statewide_stats
+    aggregate_statewide_stats
   end
+
+  def average_statewide_aggregate_stats
+    divisor = extract_aggregate_data.length
+    sum = extract_aggregate_data.inject(0){|sum,x| sum + x }
+    average_statewide_participation = sum / divisor
+  end
+
+  def set_statewide_average_as_baseline
+
+  end
+
 
 end
--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -11,8 +11,8 @@ class HeadcountAnalystTest < Minitest::Test
         :kindergarten => "./data/Kindergartners in full-day program.csv"
         }
       })
-    hc = HeadcountAnalyst.new(dr)
-    assert_instance_of HeadcountAnalyst, hc
+    ha = HeadcountAnalyst.new(dr)
+    assert_instance_of HeadcountAnalyst, ha
   end
 
   def test_average_of_district_kindergarten_participation
@@ -22,9 +22,9 @@ class HeadcountAnalystTest < Minitest::Test
         :kindergarten => "./data/Kindergartners in full-day program.csv"
         }
       })
-    hc = HeadcountAnalyst.new(dr)
-    actual = hc.kindergarten_participation_rate_variation("ACADEMY 20", nil)
-    expected = 0.4064509090909091
+    ha = HeadcountAnalyst.new(dr)
+    actual = ha.kindergarten_participation_rate_variation("ACADEMY 20", nil)
+    expected = 0.591
 
     assert_equal actual, expected
   end
@@ -36,8 +36,37 @@ class HeadcountAnalystTest < Minitest::Test
         :kindergarten => "./data/Kindergartners in full-day program.csv"
         }
       })
-    hc = HeadcountAnalyst.new(dr)
+    ha = HeadcountAnalyst.new(dr)
+    expected = ha.extract_aggregate_data
+    actual = 1991
 
-    
+    assert_equal actual, expected.length
   end
+
+  def test_average_of_statewide_stats
+    dr = DistrictRepository.new
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv"
+        }
+      })
+    ha = HeadcountAnalyst.new(dr)
+
+    actual = ha.average_statewide_aggregate_stats
+    expected = 0.6876130637870412
+
+    assert_equal actual, expected
+  end
+
+  # def test_comparison
+  #   dr = DistrictRepository.new
+  #   dr.load_data({
+  #     :enrollment => {
+  #       :kindergarten => "./data/Kindergartners in full-day program.csv"
+  #       }
+  #     })
+  #   ha = HeadcountAnalyst.new(dr)
+  #   answer = ha.kindergarten_participation_rate_variation('ACADEMY 20')
+  #
+  # end
 end
