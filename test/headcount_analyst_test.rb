1431f5d83306384abb0dabba312a0ed1445a0cbb
Padraic O'Neill <pconeill@gmail.com>
2017-04-18 14:16:22 -0600

    refactor to use actual Statewide stats

--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -23,10 +23,10 @@ class HeadcountAnalystTest < Minitest::Test
         }
       })
     ha = HeadcountAnalyst.new(dr)
-    actual = ha.kindergarten_participation_rate_variation("ACADEMY 20", nil)
-    expected = 0.591
+    actual = ha.kindergarten_participation_rate_variation("ACADEMY 20", "Colorado" )
+    expected = 0.766
 
-    assert_equal actual, expected
+    assert_equal expected, actual
   end
 
   def test_extraction_of_statewide_data
@@ -37,37 +37,27 @@ class HeadcountAnalystTest < Minitest::Test
         }
       })
     ha = HeadcountAnalyst.new(dr)
-    expected = ha.extract_aggregate_data
-    actual = 1991
-
-    assert_equal actual, expected.length
-  end
-
-  def test_average_of_statewide_stats
-    dr = DistrictRepository.new
-    dr.load_data({
-      :enrollment => {
-        :kindergarten => "./data/Kindergartners in full-day program.csv"
-        }
-      })
-    ha = HeadcountAnalyst.new(dr)
-
-    actual = ha.average_statewide_aggregate_stats
-    expected = 0.6876130637870412
+    expected = ha.average_statewide_aggregate_stats
+    actual = 0.5303936363636365
 
     assert_equal actual, expected
   end
 
-  def test_comparison
-    dr = DistrictRepository.new
-    dr.load_data({
-      :enrollment => {
-        :kindergarten => "./data/Kindergartners in full-day program.csv"
-        }
-      })
-    ha = HeadcountAnalyst.new(dr)
-    answer = ha.kindergarten_participation_rate_variation('ACADEMY 20')
-    binding.pry
-  end
+  # def test_average_of_statewide_stats
+  #   dr = DistrictRepository.new
+  #   dr.load_data({
+  #     :enrollment => {
+  #       :kindergarten => "./data/Kindergartners in full-day program.csv"
+  #       }
+  #     })
+  #   ha = HeadcountAnalyst.new(dr)
+  #
+  #   actual = ha.average_statewide_aggregate_stats
+  #   expected = 0.6876130637870412
+  #
+  #   assert_equal actual, expected
+  # end
+
+  
 
 end
